cf63ef9dee2a2219332b03be33caa77b01899350
bencyrus <mahdi.mohaghegh2001@gmail.com>
2025-07-22 14:38:40 -0400

    Initial Commit

---
+++ cue/cueApp.swift
@@ -0,0 +1,17 @@
+//
+//  cueApp.swift
+//  cue
+//
+//  Created by Ben Cyrus on 2025-07-22.
+//
+
+import SwiftUI
+
+@main
+struct cueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cueTests/cueTests.swift
@@ -0,0 +1,16 @@
+//
+//  cueTests.swift
+//  cueTests
+//
+//  Created by Ben Cyrus on 2025-07-22.
+//
+
+import Testing
+
+struct cueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
